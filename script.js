ab0eddd555a1219aabd7b52fe8d7c4f263ce56e4
Stephan Griesel <sgriesel@gmail.com>
2018-06-10 19:59:38 +0200

    update

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+// Get newsfeed
+var newsFeed = document.getElementById('newsfeed');
+
+function getNewsfeed() {
+    var newsFeedOutput = document.getElementById('newsfeed');
+    newsFeedOutput.innerHTML = '';
+}
+
 axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=424f2fb4b190440fa24b7b96038e239c')
   .then(function (response) {
     console.log(response);
   })
+
   .catch(function (error) {
     console.log(error);
   });
+
+
