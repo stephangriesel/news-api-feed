0b1543b33bb2fad1ceb274ecd66af78075c2596e
Stephan Griesel <sgriesel@gmail.com>
2018-06-12 08:35:56 +0200

    test newsitem

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-// Get newsfeed
-var newsFeed = document.getElementById('newsfeed');
+// function
+newsfeed();
 
-function getNewsfeed() {
-    var newsFeedOutput = document.getElementById('newsfeed');
-    newsFeedOutput.innerHTML = '';
+function newsfeed() {
+  var news = "Test";
+  axios
+    .get(
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=424f2fb4b190440fa24b7b96038e239c",
+      {
+        params: {
+          country: news
+        }
+      }
+    )
+    .then(function(response) {
+        // Log response
+      console.log(response.data);
+
+      // Format
+      var newsItem = response.data.articles[0].title;
+      console.log(newsItem);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 }
-
-axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=424f2fb4b190440fa24b7b96038e239c')
-  .then(function (response) {
-    console.log(response);
-  })
-
-  .catch(function (error) {
-    console.log(error);
-  });
-
-
